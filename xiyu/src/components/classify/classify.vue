7d0be3fc048b9ec439e7f01f270a85fd44496673
chuyongwyx <1525401582@qq.com>
2019-03-01 21:21:55 +0800

    分类页面重构

--- xiyu/src/components/classify/classify.vue
+++ xiyu/src/components/classify/classify.vue
@@ -2,22 +2,24 @@
 		<div class="classify">
 			<Header-com/>
 			<ContentLeft/>
+			<ConentRight/>
 		</div>
 </template>
 
 <script>
 	import Header from "./components/header"
 	import ContentLeft from "./components/cont-left"
+	import ContentRight from "./components/cont-right"
 	import Vuex from "vuex"
 	export default{
            components:{
 			   "Header-com":Header,
 			   "ContentLeft":ContentLeft,
+			   "ConentRight":ContentRight
 		   },
 		  
 		   created(){
-			   this.handleClassifyData();
-				
+			   this.handleClassifyData();	
 		   },
 		   methods:{
 			   ...Vuex.mapActions({
@@ -29,5 +31,9 @@
 </script>
 
 <style scoped lang="scss">
-
+	.classify{
+		width: 100%;
+		height: 100%;
+		display: flex;
+	}
 </style>
